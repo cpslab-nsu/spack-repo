4d763d319db3b0094793472bcc365c8f656b3464
Tahmid A. Khan <60913202+tahmid-khan@users.noreply.github.com>
2025-04-12 16:09:10 +0600

    fix(packages): several fixes and enhancements to gridpack

--- packages/gridpack/package.py
+++ packages/gridpack/package.py
@@ -2,20 +2,13 @@ from spack.package import *
 
 
 class Gridpack(CMakePackage):
-    """GridPACK is an open-source high-performance (HPC) package for simulation
-    of large-scale electrical grids.  Powered by distributed (parallel)
-    computing and high-performance numerical solvers, GridPACK offers several
-    applications forfast simulation of electrical transmission systems.
-
-    GridPACK includes a number of prebuilt applications that can be directly
-    used.  The most commonly used and well-developed are:
-      - AC Power Flow
-      - Dynamics Simulation
-      - Contingency Analysis
-
-    Other applications under development or not full featured are
-      - Dynamic security assessment
-      - State estimation
+    """GridPACK is an open-source high-performance (HPC) package for simulation of large-scale
+    electrical grids.  Powered by distributed (parallel) computing and high-performance numerical
+    solvers, GridPACK offers several applications forfast simulation of electrical transmission
+    systems.  It includes a number of prebuilt applications that can be directly used, the most
+    commonly used and well-developed of which are AC Power Flow, Dynamics Simulation, and
+    Contingency Analysis.  Other applications that are under development or not fully featured yet
+    include Dynamic Security Assessment and State Estimation.
     """
 
     homepage = "https://gridpack.pnnl.gov"
@@ -25,17 +18,18 @@ class Gridpack(CMakePackage):
 
     version("master", sha256="90faaea85a27b4e365725b7b97775f1fb550dcdd4596a24c1ce0c16d4cb5297f")
 
-    depends_on("cxx", type="build")
-    depends_on("boost +mpi +serialization +random +filesystem +system")
-    depends_on("globalarrays +cxx")
-    depends_on("mpi")
-    depends_on("petsc +metis +suite-sparse +superlu-dist")
+    patch("specify_mpi_components_explicitly.patch")
 
-    variant("shared", default=False, description="Build shared libraries")
-    variant("tests", default=False, description="Build unit tests")
+    variant("shared", default=False, description="Build dynamically linked libraries")
+    variant("tests", default=False, description="Enable build of unit tests")
     variant(
-        "test_timeout", default=120, description="Timeout for unit tests in seconds", when="+tests"
+        "test_timeout",
+        default="120",
+        description="Timeout for unit tests in seconds",
+        when="+tests",
     )
+
+    # https://github.com/GridOPTICS/GridPACK/pull/229#pullrequestreview-2679229691
     variant(
         "env_from_comm",
         default=False,
@@ -43,18 +37,27 @@ class Gridpack(CMakePackage):
         when="^globalarrays@5.9:",
     )
 
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("boost+mpi+serialization+random+filesystem+system")
+    depends_on("globalarrays+cxx")
+    depends_on("mpi")
+    depends_on("parmetis@4:")
+    depends_on("petsc+metis+suite-sparse+superlu-dist")
+
     root_cmakelists_dir = "src"
 
     def cmake_args(self):
-        args = [
-            "-DBoost_ROOT:PATH={}".format(self.spec["boost"].prefix),
-            "-DGA_DIR:PATH={}".format(self.spec["globalarrays"].prefix),
-            "-DMPI_HOME:PATH={}".format(self.spec["mpi"].prefix),
-            "-DPETSC_DIR:PATH={}".format(self.spec["petsc"].prefix),
-            self.define_from_variant("BUILD_SHARED_LIBS:BOOL", "shared"),
-            self.define_from_variant("GRIDPACK_ENABLE_TESTS:BOOL", "tests"),
-            self.define_from_variant("GRIDPACK_TEST_TIMEOUT:STRING", "test_timeout"),
-            self.define_from_variant("ENABLE_ENVIRONMENT_FROM_COMM:BOOL", "env_from_comm"),
+        return [
+            self.define("Boost_ROOT:", self.spec["boost"].prefix),
+            self.define("GA_DIR", self.spec["globalarrays"].prefix),
+            self.define("PETSC_DIR", self.spec["petsc"].prefix),
+            self.define("MPI_HOME", self.spec["mpi"].prefix),
+            self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
+            self.define_from_variant("GRIDPACK_ENABLE_TESTS", "tests"),
+            self.define_from_variant("GRIDPACK_TEST_TIMEOUT", "test_timeout"),
+            self.define_from_variant("ENABLE_ENVIRONMENT_FROM_COMM", "env_from_comm"),
         ]
 
-        return args
+    def setup_run_environment(self, env):
+        env.set("GRIDPACK_DIR", self.prefix)
